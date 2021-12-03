33b841e5bf8e294688426a1c1cc4674d4a5baac9
legyouu <80972902+legyouu@users.noreply.github.com>
2021-12-03 15:17:37 +0200

    Add files via upload

---
+++ cache/__init__.py
@@ -0,0 +1,10 @@
+# A Powerful Music Bot Property Of Muslim Indian Largest Chatting Group
+# Without Credit (Mother Fucker)
+# @L120N 
+# Owner LEGEND
+
+from cache.admins import admins, get, set
+
+__all__ = ["admins", "get", "set"]
+
+
